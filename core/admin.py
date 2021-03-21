ebe17c4e6e7fc18a076d3f134483f7b10f399cec
Ndacyayisenga-droid <noahlegend75@gmail.com>
2021-03-21 18:31:37 +0300

    new files

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Item, OderItem, Oder
+
+admin.site.register(Item)
+admin.site.register(OderItem)
+admin.site.register(Oder)
